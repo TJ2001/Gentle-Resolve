be8b47ff072b2ef4276ae3525aa340f9cad5954a
TJ2001 <jung.m.tim@gmail.com>
2016-11-18 13:29:32 -0800

    add toast for achieveButton

--- app/src/main/java/com/example/tim/gentleresolve/StartActivity.java
+++ app/src/main/java/com/example/tim/gentleresolve/StartActivity.java
@@ -5,18 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.achieveButton) Button mAchieveButton;
+    @Bind(R.id.startButton) Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Button button = (Button) findViewById(R.id.startButton);
-        button.setOnClickListener(this);
+        ButterKnife.bind(this);
+        mAchieveButton.setOnClickListener(this);
+        mStartButton.setOnClickListener(this);
     }
-        @Override
-        public void onClick(View v) {
+
+    @Override
+    public void onClick(View v){
+        if(v == mAchieveButton){
+            Toast.makeText(StartActivity.this, "This button is a work in progress.", Toast.LENGTH_LONG).show();
+        } else if (v == mStartButton){
             startActivity(new Intent(StartActivity.this, CreateActivity.class));
+        }
     }
 }
