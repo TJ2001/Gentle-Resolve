235a380b2da6a87a7659837eb4532ed07577d929
tj2001 <jung.m.tim@gmail.com>
2016-12-02 15:44:48 -0800

    mess around with listView Height

--- app/src/main/java/com/example/tim/gentleresolve/ManifestActivity.java
+++ app/src/main/java/com/example/tim/gentleresolve/ManifestActivity.java
@@ -1,13 +1,20 @@
 package com.example.tim.gentleresolve;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,11 +43,11 @@ public class ManifestActivity extends AppCompatActivity {
 
         visions = intent.getStringArrayListExtra("visions");
         Log.v(TAG, "intent: " + visions);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, visions);
-        mListView.setAdapter(adapter);
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, visions);
+//        mListView.setAdapter(adapter);
+        mListView.setAdapter(new MyListAdapter(this, R.layout.list_item, visions));
         visions.add("First Item");
         visions.add("secoond Item");
-
 //        Typeface champageFont = Typeface.createFromAsset(getAssets(), "fonts/cac_champagne.ttf");
 //        mListView.setTypeface(champageFont);
 
@@ -53,5 +60,54 @@ public class ManifestActivity extends AppCompatActivity {
 
 //        String pledge = intent.getStringExtra("vision");
 //        mPledgeView.setText(getString(R.string.agreeTo) + pledge + ".");
+    }
+
+
+    private class MyListAdapter extends ArrayAdapter<String>{
+        private int layout;
+        public MyListAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if(convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.itemTextView = (TextView) convertView.findViewById(R.id.itemTextView);
+                viewHolder.doneButton = (Button) convertView.findViewById(R.id.doneButton);
+                viewHolder.shareButton = (Button) convertView.findViewById(R.id.shareButton);
+
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (ViewHolder) convertView.getTag();
+            mainViewholder.doneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+            mainViewholder.shareButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Share button was clicked " + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            mainViewholder.itemTextView.setText(getItem(position));
+
+            return convertView;
+        }
+    }
+
+    public class ViewHolder {
+
+        ImageView thumbnail;
+        TextView itemTextView;
+        Button doneButton;
+        Button shareButton;
     }
 }
