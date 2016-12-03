0a1c9d3223a3bdb91eb8e806850e0f0bafae1926
tj2001 <jung.m.tim@gmail.com>
2016-12-02 21:49:44 -0800

    create resultListAdapter.java

--- app/src/main/java/com/example/tim/gentleresolve/ResultsListActivity.java
+++ app/src/main/java/com/example/tim/gentleresolve/ResultsListActivity.java
@@ -2,8 +2,23 @@ package com.example.tim.gentleresolve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+
+import java.security.acl.Group;
+import java.util.ArrayList;
+
+import butterknife.Bind;
 
 public class ResultsListActivity extends AppCompatActivity {
+
+    public ArrayList<Group> mGroup = new ArrayList<>();
+
+    @Bind(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    private ResultListAdapter mAdapter;
+
+    public static final String TAG = ResultsListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/tim/gentleresolve/ResultsListsAdapter.java
@@ -0,0 +1,7 @@
+package com.example.tim.gentleresolve;
+
+/**
+ * Created by Guest on 12/2/16.
+ */
+public class ResultsListsAdapter {
+}
