d4e97af65505264af2dd32228485ea3ae9b24308
TJ2001 <jung.m.tim@gmail.com>
2016-12-09 14:11:46 -0800

    add login and logout functionality

--- app/src/main/java/com/example/tim/gentleresolve/main_ui/StartActivity.java
+++ app/src/main/java/com/example/tim/gentleresolve/main_ui/StartActivity.java
@@ -3,11 +3,15 @@ package com.example.tim.gentleresolve.main_ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.tim.gentleresolve.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -32,5 +36,30 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
         } else if (v == mStartButton){
             startActivity(new Intent(StartActivity.this, CreateActivity.class));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
