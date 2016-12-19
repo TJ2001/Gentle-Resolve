da7cb427af23447ff5364bc15ef0b02a4b61d3c9
TJ2001 <jung.m.tim@gmail.com>
2016-12-19 00:49:06 -0800

    merge changes from foxtrot

--- app/src/main/java/com/example/tim/gentleresolve/main_ui/ManifestActivity.java
+++ app/src/main/java/com/example/tim/gentleresolve/main_ui/ManifestActivity.java
@@ -3,7 +3,9 @@ package com.example.tim.gentleresolve.main_ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,29 +18,42 @@ import com.example.tim.gentleresolve.models.Vision;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ManifestActivity extends AppCompatActivity{
-    private DatabaseReference mVisionReference;
+public class ManifestActivity extends AppCompatActivity {
     private FirebaseListAdapter<Vision> mVision;
+    private DatabaseReference mVisionReference;
+    private FirebaseListAdapter<Vision> mVisionFirebaseListAdapter;
     private ValueEventListener mVisionReferenceListener;
+    private ChildEventListener mChildEventListener;
+    private ArrayList<Vision> mVisions = new ArrayList<>();
 
-    @Bind(R.id.visionListView) ListView mListView;
-    @Bind(R.id.supportButton) Button mSupportButton;
+    @Bind(R.id.supportButton)
+    Button mSupportButton;
+    @Bind(R.id.visionListView)
+    ListView mListView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manifest);
         ButterKnife.bind(this);
 
+        setUpFirebaseAdapter();
+    }
+
+
+    private void setUpFirebaseAdapter() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
 
@@ -47,26 +62,54 @@ public class ManifestActivity extends AppCompatActivity{
                 .getReference(Constants.FIREBASE_CHILD_VISIONS)
                 .child(uid);
 
-        mVisionReferenceListener = mVisionReference.addValueEventListener(new ValueEventListener() {
+        mChildEventListener = mVisionReference.addChildEventListener(new ChildEventListener() {
 
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {}
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                mVisions.add(dataSnapshot.getValue(Vision.class));
+            }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {}
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
         });
 
-        mVision = new FirebaseListAdapter<Vision>(this, Vision.class, R.layout.list_item, mVisionReference) {
+
+        mVisionFirebaseListAdapter = new FirebaseListAdapter<Vision>(this, Vision.class, R.layout.list_item, mVisionReference) {
 
             @Override
             protected void populateView(View v, Vision model, int position) {
-                ((TextView)v.findViewById(R.id.itemTextView)).setText(model.getName());
+                ((TextView) v.findViewById(R.id.itemTextView)).setText(model.getName());
 
                 model.setPushId(getRef(position).getKey());
             }
         };
 
-        mListView.setAdapter(mVision);
+        mListView.setAdapter(mVisionFirebaseListAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.v("LogV", "postion " + position);
+                mVisionReference.removeValue();
+            }
+        });
 
         mSupportButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,9 +122,7 @@ public class ManifestActivity extends AppCompatActivity{
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mVisionReference.removeEventListener(mVisionReferenceListener);
+        mVisionReference.removeEventListener(mChildEventListener);
+
     }
 }
-
-
-
